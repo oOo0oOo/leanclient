3ff15667881fb8aa2a9ef4b0be93a784a4c06d1d
Oliver Dressler <odressler@gmail.com>
2025-01-03 01:01:35 +0100

    Refactor SemanticTokenProcessor

--- leanclient/language_server.py
+++ leanclient/language_server.py
@@ -13,6 +13,7 @@ from leanclient.config import (
     LAKE_ENV_DIR,
 )
 from leanclient.env_setup import install_env
+from leanclient.utils import SemanticTokenProcessor
 
 
 class LeanLanguageServer:
@@ -74,11 +75,9 @@ class LeanLanguageServer:
 
         # Send initialization request, surprisingly no params required
         results = self._send_request("initialize", {"processId": os.getpid()})
-        self.server_info = results[-1]["result"]
-        self.token_legend = self.server_info["capabilities"]["semanticTokensProvider"][
-            "legend"
-        ]
-        self.num_token_modifiers = len(self.token_legend["tokenModifiers"])
+        server_info = results[-1]["result"]
+        legend = server_info["capabilities"]["semanticTokensProvider"]["legend"]
+        self.token_processor = SemanticTokenProcessor(legend)
 
         self._send_notification("initialized", {})
 
@@ -352,7 +351,7 @@ class LeanLanguageServer:
 
     def request_semantic_tokens_full(self, uri: str) -> list:
         res = self.send_request_document(uri, "textDocument/semanticTokens/full", {})
-        return self._process_semantic_tokens(res["data"])
+        return self.token_processor(res["data"])
 
     def request_semantic_tokens_range(
         self,
@@ -372,49 +371,7 @@ class LeanLanguageServer:
                 }
             },
         )
-        return self._process_semantic_tokens(res["data"])
-
-    def _process_semantic_tokens(self, raw_response: list[int]) -> list:
-        """Semantic token response is "compressed". This function is a reverse translation of:
-        https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#semanticTokens_fullRequest
-        """
-        tokens = []
-        prev_line = 0
-        prev_start_char = 0
-        for i in range(0, len(raw_response), 5):
-            delta_line = raw_response[i]
-            delta_start_char = raw_response[i + 1]
-            length = raw_response[i + 2]
-            token_type = raw_response[i + 3]
-            token_modifiers = raw_response[i + 4]
-
-            line = prev_line + delta_line
-            start_char = (
-                prev_start_char + delta_start_char
-                if delta_line == 0
-                else delta_start_char
-            )
-
-            token_modifiers_list = [
-                self.token_legend["tokenModifiers"][j]
-                for j in range(self.num_token_modifiers)
-                if token_modifiers & (1 << j)
-            ]
-
-            tokens.append(
-                [
-                    line,
-                    start_char,
-                    length,
-                    self.token_legend["tokenTypes"][token_type],
-                    token_modifiers_list,
-                ]
-            )
-
-            prev_line = line
-            prev_start_char = start_char
-
-        return tokens
+        return self.token_processor(res["data"])
 
     def request_folding_range(self, uri: str) -> dict:
         return self.send_request_document(uri, "textDocument/foldingRange", {})
--- leanclient/utils.py
+++ leanclient/utils.py
@@ -12,6 +12,60 @@ def find_lean_files_recursively(abs_path: str) -> list[str]:
     return uris
 
 
+class SemanticTokenProcessor:
+    def __init__(self, token_legend: dict):
+        self.token_types = token_legend["tokenTypes"]
+        self.token_modifiers = token_legend["tokenModifiers"]
+        self.num_token_modifiers = len(token_legend["tokenModifiers"])
+
+    def __call__(self, raw_response: list[int]) -> list:
+        return self._process_semantic_tokens(raw_response)
+
+    def _process_semantic_tokens(self, raw_response: list[int]) -> list:
+        """Semantic token response is converted using the token_legend
+
+        This function is a reverse translation of:
+        https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#semanticTokens_fullRequest
+        """
+        tokens = []
+        prev_line = 0
+        prev_start_char = 0
+        for i in range(0, len(raw_response), 5):
+            delta_line = raw_response[i]
+            delta_start_char = raw_response[i + 1]
+            length = raw_response[i + 2]
+            token_type = raw_response[i + 3]
+            token_modifiers = raw_response[i + 4]
+
+            line = prev_line + delta_line
+            start_char = (
+                prev_start_char + delta_start_char
+                if delta_line == 0
+                else delta_start_char
+            )
+
+            token_modifiers_list = [
+                self.token_modifiers[j]
+                for j in range(self.num_token_modifiers)
+                if token_modifiers & (1 << j)
+            ]
+
+            tokens.append(
+                [
+                    line,
+                    start_char,
+                    length,
+                    self.token_types[token_type],
+                    token_modifiers_list,
+                ]
+            )
+
+            prev_line = line
+            prev_start_char = start_char
+
+        return tokens
+
+
 # Profiling
 def start_profiler() -> cProfile.Profile:
     sys.setrecursionlimit(10000)
